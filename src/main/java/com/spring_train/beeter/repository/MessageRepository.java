68b89f314a0df9e192db847899ddf11898a1e4a3
Alex Krechet <rubassabaka88@gmail.com>
2019-11-03 16:28:52 +0200

    DB coonector and CRUD operations were implemented

---
+++ src/main/java/com/spring_train/beeter/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.spring_train.beeter.repository;
+
+import com.spring_train.beeter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
